4e5cff9675537422af1dfed10655634f0eb2d313
NatanFreitas <115927951+MrNatan2023@users.noreply.github.com>
2024-05-27 18:51:51 -0300

    Retirei o ProdutoLimpeza

--- src/main/java/com/lavajato/LavaJato_2/repository/ProdutoLimpezaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.lavajato.LavaJato_2.repository;
-
-import com.lavajato.LavaJato_2.entities.ProdutoLimpeza;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProdutoLimpezaRepository extends JpaRepository<ProdutoLimpeza, Integer> {
-}
