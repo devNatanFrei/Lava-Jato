ea9df299cf894561040b15f493fb9dfbb08af33d
NatanFreitas <115927951+MrNatan2023@users.noreply.github.com>
2024-06-04 19:50:07 -0300

    ajeitei o nome

--- src/main/java/com/lavajato/LavaJato_2/controller/ControllerProdutoLimpeza.java
+++ src/main/java/com/lavajato/LavaJato_2/controller/ControllerProdutoLimpeza.java
@@ -9,12 +9,12 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/produtos")
-class ProdutoLimpezaController {
+class ControllerProdutoLimpeza {
 
     private final ProdutoLimpezaService produtoLimpezaService;
 
     @Autowired
-    public ProdutoLimpezaController(ProdutoLimpezaService produtoLimpezaService) {
+    public ControllerProdutoLimpeza(ProdutoLimpezaService produtoLimpezaService) {
         this.produtoLimpezaService = produtoLimpezaService;
     }
 
