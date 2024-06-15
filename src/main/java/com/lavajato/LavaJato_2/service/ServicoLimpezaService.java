2a1f491fd236347f13c0de235dcd3889159f85a6
NatanFreitas <115927951+MrNatan2023@users.noreply.github.com>
2024-06-15 12:03:28 -0300

    fiz só algumas mudanças irrelevantes

--- src/main/java/com/lavajato/LavaJato_2/service/ServicoLimpezaService.java
+++ src/main/java/com/lavajato/LavaJato_2/service/ServicoLimpezaService.java
@@ -2,10 +2,13 @@ package com.lavajato.LavaJato_2.service;
 
 import com.lavajato.LavaJato_2.entities.ServicoLimpeza;
 import com.lavajato.LavaJato_2.repository.ServicoLimpezaRepository;
+import com.lavajato.LavaJato_2.entities.Cliente;
+import com.lavajato.LavaJato_2.entities.ProdutoLimpeza;
+import com.lavajato.LavaJato_2.repository.ClienteRepository;
+import com.lavajato.LavaJato_2.repository.ProdutoLimpezaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -13,10 +16,14 @@ import java.util.Optional;
 public class ServicoLimpezaService {
 
     private final ServicoLimpezaRepository servicoLimpezaRepository;
+    private final ClienteRepository clienteRepository;
+    private final ProdutoLimpezaRepository produtoLimpezaRepository;
 
     @Autowired
-    public ServicoLimpezaService(ServicoLimpezaRepository servicoLimpezaRepository) {
+    public ServicoLimpezaService(ServicoLimpezaRepository servicoLimpezaRepository, ClienteRepository clienteRepository, ProdutoLimpezaRepository produtoLimpezaRepository) {
         this.servicoLimpezaRepository = servicoLimpezaRepository;
+        this.clienteRepository = clienteRepository;
+        this.produtoLimpezaRepository = produtoLimpezaRepository;
     }
 
     public ServicoLimpeza criarServicoLimpeza(ServicoLimpeza servicoLimpeza) {
@@ -29,8 +36,10 @@ public class ServicoLimpezaService {
             ServicoLimpeza updatedServico = existingServico.get();
             updatedServico.setDataHora(servicoLimpeza.getDataHora());
             updatedServico.setTipoServico(servicoLimpeza.getTipoServico());
-            updatedServico.setCliente(servicoLimpeza.getCliente());
-            updatedServico.setProdutoLimpeza(servicoLimpeza.getProdutoLimpeza());
+
+            updatedServico.setClienteId(servicoLimpeza.getClienteId());
+            updatedServico.setProdutoLimpezaId(servicoLimpeza.getProdutoLimpezaId());
+
             return servicoLimpezaRepository.save(updatedServico);
         } else {
             throw new RuntimeException("Serviço de limpeza não encontrado.");
@@ -49,4 +58,11 @@ public class ServicoLimpezaService {
         return servicoLimpezaRepository.findById(id);
     }
 
+    public Cliente obterClientePorId(Integer clienteId) {
+        return clienteRepository.findById(clienteId).orElse(null);
+    }
+
+    public ProdutoLimpeza obterProdutoLimpezaPorId(Integer produtolimpezaId) {
+        return produtoLimpezaRepository.findById(produtolimpezaId).orElse(null);
+    }
 }
